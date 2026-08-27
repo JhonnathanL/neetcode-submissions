85310e499645faec0384997bb5126719be33e2c1
Jhonnathan de Sousa Pessoa <91705066+JhonnathanL@users.noreply.github.com>
2026-08-27 12:13:46 -0300

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        curr_max = nums[0]
+        curr_min = nums[0]
+        res = nums[0]
+
+        for num in nums[1:]:
+
+            old_max = curr_max
+
+            curr_max = max(
+                num,
+                num * curr_max,
+                num * curr_min
+            )
+
+            curr_min = min(
+                num,
+                num * old_max,
+                num * curr_min
+            )
+
+            res = max(res, curr_max)
+
+        return res
