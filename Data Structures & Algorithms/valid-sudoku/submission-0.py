35b1e55e8fde63871932117f49304081cd56fbce
Jhonnathan de Sousa Pessoa <91705066+JhonnathanL@users.noreply.github.com>
2026-08-28 18:33:31 -0300

    Clean up comments in valid-sudoku submission
    Removed comments in Portuguese for clarity.

--- Data Structures & Algorithms/valid-sudoku/submission-0.py
+++ Data Structures & Algorithms/valid-sudoku/submission-0.py
@@ -4,26 +4,22 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
 
-        # Linhas e colunas
         for r in range(rows):
             seen_row = set()
             seen_col = set()
 
             for c in range(cols):
 
-                # Linha
                 if board[r][c] in seen_row and board[r][c] != ".":
                     return False
 
                 seen_row.add(board[r][c])
 
-                # Coluna
                 if board[c][r] in seen_col and board[c][r] != ".":
                     return False
 
                 seen_col.add(board[c][r])
 
-        # Quadrados 3x3
         for r in range(0, rows, 3):
             for c in range(0, cols, 3):
 
