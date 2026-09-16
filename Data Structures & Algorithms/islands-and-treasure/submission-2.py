3f589d4b471049284db0fe32b7cfd60b1afe18e3
Jhonnathan de Sousa Pessoa <91705066+JhonnathanL@users.noreply.github.com>
2026-09-16 14:45:53 -0300

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        queue = deque()
+
+        def bfs(queue):
+            
+            directions = [
+                [1,0],
+                [-1, 0],
+                [0, 1],
+                [0, -1]
+            ]
+
+            while queue:
+                r, c = queue.popleft()
+
+                for dr, dc in directions:
+                    nr = dr + r
+                    nc = dc + c
+
+                    if (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 2147483647):
+                        queue.append((nr, nc))
+                        grid[nr][nc] = grid[r][c] + 1
+
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    queue.append((i,j))
+        
+        bfs(queue)
+
